243a4684b25890fac65c94ed7eda856108dc3e15
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-11-16 00:34:47 -0300

    Creating Java Project

---
+++ Java_GlobalSolution_LessPollu/LessPollu/src/main/java/com/GlobalSolution/LessPollu/LessPolluApplication.java
@@ -0,0 +1,13 @@
+package com.GlobalSolution.LessPollu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessPolluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessPolluApplication.class, args);
+	}
+
+}
---
+++ Java_GlobalSolution_LessPollu/LessPollu/src/test/java/com/GlobalSolution/LessPollu/LessPolluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GlobalSolution.LessPollu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessPolluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
